57837cb5d848ae0fc15b35c5530da5f7e4479cbe
Teddy Choi <pudidic@gmail.com>
2018-11-06 13:15:32 +0900

    Unrolled and chained Murmur32 without byte array

--- src/main/java/org/notmysock/hive/udf/GenericMurmurLong2HashUdf.java
+++ src/main/java/org/notmysock/hive/udf/GenericMurmurLong2HashUdf.java
@@ -13,6 +13,14 @@ import org.apache.hive.common.util.Murmur3;
 
 public class GenericMurmurLong2HashUdf extends GenericUDF {
 
+  private static final int C1_32 = -862048943;
+  private static final int C2_32 = 461845907;
+  private static final int R1_32 = 15;
+  private static final int R2_32 = 13;
+  private static final int M_32 = 5;
+  private static final int N_32 = -430675100;
+  private static final int DEFAULT_SEED = 104729;
+
   private transient PrimitiveCategory[] inputTypes = new PrimitiveCategory[2];
   private transient Converter[] converters = new Converter[2];
   private transient IntWritable output = new IntWritable();
@@ -42,17 +50,96 @@ public class GenericMurmurLong2HashUdf extends GenericUDF {
   public Object evaluate(DeferredObject[] arguments) throws HiveException {
     final long l0 = getLongValue(arguments, 0, converters);
     final long l1 = getLongValue(arguments, 1, converters);
-    /*
-     * 2x murmur + XOR + cast (lazy, but fix this with a chained Murmur32)
-     */
-    final int h = (int) (Murmur3.hash64(l0) ^ Murmur3.hash64(l1));
+
+    final int h = hash32(l0, l1);
     output.set(h);
     return output;
+  }
+
+  public static int hash32(long l0, long l1) {
+    return hash32(l0, l1, DEFAULT_SEED);
+  }
+
+  public static int hash32(long l0, long l1, int seed) {
+    int hash = seed;
+
+    final byte  b0 = (byte) (l0 >> 56);
+    final byte  b1 = (byte) (l0 >> 48);
+    final byte  b2 = (byte) (l0 >> 40);
+    final byte  b3 = (byte) (l0 >> 32);
+    final byte  b4 = (byte) (l0 >> 24);
+    final byte  b5 = (byte) (l0 >> 16);
+    final byte  b6 = (byte) (l0 >>  8);
+    final byte  b7 = (byte) (l0 >>  0);
+    final byte  b8 = (byte) (l1 >> 56);
+    final byte  b9 = (byte) (l1 >> 48);
+    final byte b10 = (byte) (l1 >> 40);
+    final byte b11 = (byte) (l1 >> 32);
+    final byte b12 = (byte) (l1 >> 24);
+    final byte b13 = (byte) (l1 >> 16);
+    final byte b14 = (byte) (l1 >>  8);
+    final byte b15 = (byte) (l1 >>  0);
+
+    // body
+    int k;
+
+    // Roll 1
+    k = (b0 & 0xff)
+        | ((b1 & 0xff) << 8)
+        | ((b2 & 0xff) << 16)
+        | ((b3 & 0xff) << 24);
+    k *= C1_32;
+    k = Integer.rotateLeft(k, R1_32);
+    k *= C2_32;
+    hash ^= k;
+    hash = Integer.rotateLeft(hash, R2_32) * M_32 + N_32;
+
+    // Roll 2
+    k = (b4 & 0xff)
+        | ((b5 & 0xff) << 8)
+        | ((b6 & 0xff) << 16)
+        | ((b7 & 0xff) << 24);
+    k *= C1_32;
+    k = Integer.rotateLeft(k, R1_32);
+    k *= C2_32;
+    hash ^= k;
+    hash = Integer.rotateLeft(hash, R2_32) * M_32 + N_32;
+
+    // Roll 3
+    k = (b8 & 0xff)
+        | ((b9 & 0xff) << 8)
+        | ((b10 & 0xff) << 16)
+        | ((b11 & 0xff) << 24);
+    k *= C1_32;
+    k = Integer.rotateLeft(k, R1_32);
+    k *= C2_32;
+    hash ^= k;
+    hash = Integer.rotateLeft(hash, R2_32) * M_32 + N_32;
+
+    // Roll 4
+    k = (b12 & 0xff)
+        | ((b13 & 0xff) << 8)
+        | ((b14 & 0xff) << 16)
+        | ((b15 & 0xff) << 24);
+    k *= C1_32;
+    k = Integer.rotateLeft(k, R1_32);
+    k *= C2_32;
+    hash ^= k;
+    hash = Integer.rotateLeft(hash, R2_32) * M_32 + N_32;
+
+    // finalization
+    hash ^= 16;
+    hash ^= (hash >>> 16);
+    hash *= 0x85ebca6b;
+    hash ^= (hash >>> 13);
+    hash *= 0xc2b2ae35;
+    hash ^= (hash >>> 16);
+
+    return hash;
   }
 
   @Override
   public String getDisplayString(String[] children) {
     return getStandardDisplayString("long2hash", children, ",");
   }
-
 }
